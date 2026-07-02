bf1ca586b14c438975a5ef006002877b2385d165
Joao Victor <ottojvn@proton.me>
2026-07-02 10:05:25 -0300

    chore: implement exercise 1.6

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,41 +1,36 @@
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
+const Header = ({ name }) => (<h1>{name}</h1>);
 
-const Content = ({ modules }) => {
-  return (
-    <div>
-      <Part module={modules[0]} />
-      <Part module={modules[1]} />
-      <Part module={modules[2]} />
-    </div>
-  );
-};
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part, idx) => (
+      <Part key={idx} part={part} />
+    ))}
+  </div>
+);
 
-const Part = ({ module }) => {
-  return (
-    <p>
-      {module.name} {module.exercises}
-    </p>
-  );
-};
-const Total = ({ modules }) => {
-  const sum = modules.reduce((sum, module) => sum + module.exercises, 0);
-  return <p>Number of exercises {sum}</p>;
-};
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Total = ({ parts }) => (<p>Number of exercises {parts.reduce((sum, module) => sum + module.exercises, 0)}</p>);
+
 const App = () => {
-  const course = { name: "Half Stack application development" };
-  const modules = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ]
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content modules={modules} />
-      <Total modules={modules} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (setter, value) => setter(value + 1);
+
+  return (
+    <div>
+      <div>
+        <h1>give feedback</h1>
+        <button onClick={() => handleClick(setGood, good)}>good</button>
+        <button onClick={() => handleClick(setNeutral, neutral)}>neutral</button>
+        <button onClick={() => handleClick(setBad, bad)}>bad</button>
+      </div>
+      <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    </div>
+  )
+}
+
+export default App

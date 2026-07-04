626a05196c94c0f49a4295545bfc8a245f8ffbc2
Joao Victor <ottojvn@proton.me>
2026-07-04 09:31:51 -0300

    chore: implement exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import axios from 'axios'
+
+import { useState, useEffect } from 'react'
 import { Numbers } from './components/Numbers'
 import { Filter } from './components/Filter'
 import { Form } from './components/Form'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -35,9 +38,18 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter value={filterName} onChange={(e) => setFilterName(e.target.value)} />
-      <Form name={newName} number={newNumber} onNameChange={e => setNewName(e.target.value)} onNumberChange={(e => setNewNumber(e.target.value))} onSubmit={handleSubmit} />
-      <Numbers persons={persons} filterName={filterName} />
+      <Filter
+        value={filterName}
+        onChange={(e) => setFilterName(e.target.value)} />
+      <Form
+        name={newName}
+        number={newNumber}
+        onNameChange={e => setNewName(e.target.value)}
+        onNumberChange={(e => setNewNumber(e.target.value))}
+        onSubmit={handleSubmit} />
+      <Numbers
+        persons={persons}
+        filterName={filterName} />
     </div>
   )
 }

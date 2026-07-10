996b91f0a177cb5e7ff488da33df7a66b1f21ba8
Joao Victor <ottojvn@proton.me>
2026-07-10 02:09:33 -0300

    feat: implement delete '/api/person:id' endpoint

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -48,4 +48,12 @@ app.get('/api/persons/:id', (request, response) => {
 
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    entries = entries.filter(entry => {
+        entry.id !== request.params.id
+    })
+
+    response.status(204).end()
+})
+
 app.listen(3000)

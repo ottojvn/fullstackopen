f4f0edeced7ac9bd18586111a71c9a4d3a294c2b
Joao Victor <ottojvn@proton.me>
2026-07-13 15:33:36 -0300

    phonebook: add build scripts

--- part3/phonebook/api/index.js
+++ part3/phonebook/api/index.js
@@ -94,6 +94,7 @@ app.post('/api/persons', (request, response) => {
     response.status(201).send(person)
 })
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000')
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
